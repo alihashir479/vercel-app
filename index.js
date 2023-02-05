a2de5571147b6447dcfc21f979e781c64228ea04
Hashir <alihashir479@gmail.com>
2023-02-05 08:46:28 +0500

    vercel changes

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ import express from 'express'
 const app = express()
 
 
-app.get('/', () => {
+app.get('/', (req,res) => {
     res.send('vercel res')
 })
+
 app.listen(5000, () => {
     console.log('app is running')
 })
